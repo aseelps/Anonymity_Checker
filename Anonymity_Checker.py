d1e418d92584391aae5474e89faf81d7d4387d63
Aseel P Sathar <49149920+aseelps@users.noreply.github.com>
2019-11-16 18:42:31 +0530

    Add files via upload

---
+++ Anonymity_Checker.py
@@ -0,0 +1,27 @@
+import requests
+
+res = requests.get('https://ipinfo.io/')
+data = res.json()
+
+country = data['country']
+timezone = data['timezone']
+ip = data['ip']
+postal = data['postal']
+city = data['city']
+region = data['region']
+location = data['loc'].split(',')
+latitude = location[0]
+longitude = location[1]
+print("\n")
+print("Anonymity Checker ")
+print("\t Tool Designed by aseelps")
+print("\n")
+print("IP : ", ip)
+print("Country : ", country)
+print("Region : ", region)
+print("City : ", city)
+print("Postal : ", postal)
+print("Timezone : ", timezone)
+print("Latitude : ", latitude)
+print("Longitude : ", longitude)
+
